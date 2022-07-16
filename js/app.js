3bda9dfc6b63e952557c23cab46854882024f0f6
Alexander <101967630+Egorov-design@users.noreply.github.com>
2022-07-16 08:11:15 +0300

    add img files and style.scss change

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$(".slider").slick({
+		arrows: true, //Убирает или включает стрелки
+		dots: true, //Убирает или включает точки
+		fade: false, //Затухание
+		adaptiveHeight: true,
+		infinite: false,
+		edgeFriction: 0.15,
+		// autoplay:true,
+		// autoplaySpeed:2000
+	});
+});
